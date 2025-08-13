88f3f78adf71bfc8aae1d9a15c3b9a6e262501fd
Josh <josh@example.com>
2025-08-13 09:37:56 +0000

    styling, skeletons, filtering

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -2,15 +2,23 @@ class CommunityController < ApplicationController
   def index
     @categories = CommunityCategory.joins(:spaces).distinct
 
-    selected_category_slug = params[:tab]
-    selected_category = CommunityCategory.find_by(slug: selected_category_slug) if selected_category_slug.present?
-    selected_age_group_slug = params[:age_group]
-    selected_age_group = AgeGroupCategory.find_by(slug: selected_age_group_slug) if selected_age_group_slug.present?
+    # Set defaults if no parameters are present
+    params[:tab] = 'all' if params[:tab].blank?
+    params[:age_group] = 'all' if params[:age_group].blank?
+
+    # Handle single category selection
+    selected_category = CommunityCategory.find_by(slug: params[:tab]) if params[:tab].present? && params[:tab] != 'all'
+
+    # Handle single age group selection  
+    selected_age_group = AgeGroupCategory.find_by(slug: params[:age_group]) if params[:age_group].present? && params[:age_group] != 'all'
 
     @spaces = Space.includes(:community_categories, :age_group_categories, :memberships)
+    
+    # Apply filters based on selections
     if selected_category && selected_age_group
       @spaces = @spaces.joins(:community_categories, :age_group_categories)
-                       .where(community_categories: { id: selected_category.id }, age_group_categories: { id: selected_age_group.id })
+                       .where(community_categories: { id: selected_category.id })
+                       .where(age_group_categories: { id: selected_age_group.id })
     elsif selected_category
       @spaces = @spaces.joins(:community_categories)
                        .where(community_categories: { id: selected_category.id })
@@ -20,6 +28,7 @@ class CommunityController < ApplicationController
     end
     @spaces = @spaces.order(created_at: :desc).distinct
 
+    # Age groups available based on selected categories
     if selected_category
       @age_groups = AgeGroupCategory.joins(spaces: :community_categories)
                                    .where(community_categories: { id: selected_category.id })
@@ -28,15 +37,20 @@ class CommunityController < ApplicationController
       @age_groups = AgeGroupCategory.joins(:spaces).distinct
     end
 
+    # Joined spaces filtered by selected categories
     if current_user
       @joined_spaces = current_user.spaces
-      @joined_spaces = @joined_spaces.joins(:community_categories).where(community_categories: { id: selected_category.id }) if selected_category
+      if selected_category
+        @joined_spaces = @joined_spaces.joins(:community_categories)
+                                      .where(community_categories: { id: selected_category.id })
+      end
       @joined_spaces = @joined_spaces.distinct
     else
       @joined_spaces = []
     end
   end
 end
+
 
 
 

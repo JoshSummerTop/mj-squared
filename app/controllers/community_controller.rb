009f087138b4a6e8aeffc803fd4a76f1f149c146
Josh <josh@example.com>
2025-08-15 12:35:30 +0000

    fix filtering

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -7,8 +7,8 @@ class CommunityController < ApplicationController
     params[:age_group] = 'all' if params[:age_group].blank?
 
     # Clean pagination setup
-    @selected_category = CommunityCategory.find_by(slug: params[:tab]) if params[:tab].present? && params[:tab] != 'all'
-    @selected_age_group = AgeGroupCategory.find_by(slug: params[:age_group]) if params[:age_group].present? && params[:age_group] != 'all'
+    @selected_category = CommunityCategory.find_by(slug: params[:tab]) if params[:tab] != 'all'
+    @selected_age_group = AgeGroupCategory.find_by(slug: params[:age_group]) if params[:age_group] != 'all'
 
     # Manual pagination (bypassing pagy configuration issues)
     page = (params[:page] || 1).to_i
@@ -34,9 +34,28 @@ class CommunityController < ApplicationController
       @age_groups = AgeGroupCategory.joins("INNER JOIN age_group_categories_spaces ags ON ags.age_group_category_id = age_group_categories.id")
                                    .joins("INNER JOIN community_categories_spaces ccs ON ccs.space_id = ags.space_id") 
                                    .where("ccs.community_category_id = ?", @selected_category.id)
+                                   .select("age_group_categories.*, CASE name 
+                                           WHEN 'Early Intervention (0-3)' THEN 1
+                                           WHEN 'Preschool (3-5)' THEN 2
+                                           WHEN 'Elementary (6-11)' THEN 3
+                                           WHEN 'Middle School (12-14)' THEN 4
+                                           WHEN 'High School (15-18)' THEN 5
+                                           WHEN 'Young Adults (18+)' THEN 6
+                                           ELSE 7
+                                         END as sort_order")
                                    .distinct
+                                   .order(Arel.sql("sort_order"))
     else
-      @age_groups = AgeGroupCategory.all
+      @age_groups = AgeGroupCategory.all.select("age_group_categories.*, CASE name 
+                                               WHEN 'Early Intervention (0-3)' THEN 1
+                                               WHEN 'Preschool (3-5)' THEN 2
+                                               WHEN 'Elementary (6-11)' THEN 3
+                                               WHEN 'Middle School (12-14)' THEN 4
+                                               WHEN 'High School (15-18)' THEN 5
+                                               WHEN 'Young Adults (18+)' THEN 6
+                                               ELSE 7
+                                             END as sort_order")
+                                   .order(Arel.sql("sort_order"))
     end
 
     # Joined spaces filtered by selected categories
@@ -65,7 +84,6 @@ class CommunityController < ApplicationController
     # Apply filters using clean approach
     spaces = spaces.joins(:community_categories).where(community_categories: {id: @selected_category.id}) if @selected_category
     spaces = spaces.joins(:age_group_categories).where(age_group_categories: {id: @selected_age_group.id}) if @selected_age_group
-
 
     spaces.distinct
   end
